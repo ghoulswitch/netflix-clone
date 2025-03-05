9a7b733ea53e645caf687ada79cdaea7c0b1fe3c
ghoulswitch <die.millyturn@gmail.com>
2025-03-05 16:59:53 -0300

    página inicial ✅ + ajustes de estilo + ajuste de funções

--- src/script/script-index.js
+++ src/script/script-index.js
@@ -1,3 +1,7 @@
 function abrirPaginaInicio() {
-    window.location.href = './src/html/inicio.html';
+    window.location.href = '/src/html/inicio.html';
+}
+
+function entrarPaginaPerfis() {
+    window.location.href = './src/html/perfis.html';
 }
